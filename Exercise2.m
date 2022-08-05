04d4e68083ffb6cfdbc7abd914e9bb20073abd47
Mc Angelo R. Lacson <76871216+penguinboy2829@users.noreply.github.com>
2022-08-05 21:44:01 +0800

    Add files via upload

---
+++ Exercise2.m
@@ -0,0 +1,19 @@
+%Open fruits.png
+sample1 = imread('/Users/mcangelolacson/Desktop/Elective 3/fruits.png');
+imshow(sample1);
+figure(1),imshow(sample1);
+
+%Check data type
+imfinfo 'fruits.png' 
+whos sample1; %data type/class = uint8
+
+%Reduce Spatial Resolution
+sample2 = imresize (sample1/2, 0.5);
+imwrite(sample2,'/Users/mcangelolacson/Desktop/Elective 3/fruits2.png');
+figure(2),imshow(sample2);
+
+%Convert RGB to HSV
+sample3 = rgb2hsv(sample1);
+imwrite(sample3,'/Users/mcangelolacson/Desktop/Elective 3/fruits3.png');
+figure(3),imshow(sample3);
+
